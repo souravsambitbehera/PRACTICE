1f8052dfd24bdedf41ab4233a8bcaf67babf14dc
Sourav Sambit Behera <souravsambit12@gmail.com>
2022-11-30 23:25:15 +0530

    commit

---
+++ day4.js
@@ -0,0 +1,33 @@
+// var changeLi = document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild
+// changeLi.innerHTML = "sourav"
+
+// javscript practice
+// var str = "123"
+// console.log(typeof(str))
+/*
+let str ="sourav"
+let num = 5
+console.log(str+num)
+// const str = "123"
+console.log(typeof(str+num))*/
+// ques 1
+let str = " sourav"
+let num = 6
+document.write(str+num,"<br>");
+
+// que 2
+document.write(typeof(str+num,"<br>"));
+document.write("<br>")
+
+// ques 3
+
+const stu = {
+    sourav :  "123",
+    sambit : '233',
+    babu : '3233'
+
+}
+document.write(stu.sourav)
+// const stu =4 (you can't re assign const value)
+stu["name"] = "any"
+document.write(stu[2])
