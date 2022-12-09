27c2125868647824800ec0367501ec8813a148f9
Sourav Sambit Behera <souravsambit12@gmail.com>
2022-12-09 21:58:22 +0530

    commit

--- Advance JS/index.js
+++ Advance JS/index.js
@@ -1,12 +1,37 @@
-const cars = ["BMW", "Volvo", "Saab", "Ford"];
-let text = "";
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let text = "";
 
-list: {
-  text += cars[0] + "<br>"; 
-  text += cars[1] + "<br>"; 
-  break list;
-  text += cars[2] + "<br>"; 
-  text += cars[3] + "<br>"; 
-}
+// list: {
+//   text += cars[0] + "<br>"; 
+//   text += cars[1] + "<br>"; 
+//   break list;
+//   text += cars[2] + "<br>"; 
+//   text += cars[3] + "<br>"; 
+// }
 
-document.getElementById("list").innerHTML = text;
+// document.getElementById("list").innerHTML = text;
+
+// const fruits = new Map([
+//     ["apples", 500],
+//     ["bananas", 300],
+//     ["oranges", 200]
+//   ]);
+  
+//   document.getElementById("demo").innerHTML = fruits.get("bananas");
+
+
+function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try { 
+      if(x == "")  throw "empty";
+      if(isNaN(x)) throw "not a number";
+      x = Number(x);
+      if(x < 5)  throw "too low";
+      if(x > 10)   throw "too high";
+    }
+    catch(err) {
+      message.innerHTML = "Input is " + err;
+    }
+  }
