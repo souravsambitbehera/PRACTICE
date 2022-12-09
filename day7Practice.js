7ea6437efc33bc53fac74f5bfb7a785c1ca49410
Sourav Sambit Behera <souravsambit12@gmail.com>
2022-12-09 10:10:58 +0530

    commit

--- day7Practice.js
+++ day7Practice.js
@@ -1,18 +1,24 @@
-// Question1
+// // Question1
 
-// let x = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild
-// x.style.color ="red"
-// x.getElementsByClassName.color = "red"
+// // let x = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild
+// // x.style.color ="red"
+// // x.getElementsByClassName.color = "red"
+// // console.log(x)
+
+// // // question 2
+
+// // let x = document.getElementsByClassName("ptag")//.style.color="green"
+
+// // x[0].style.color = "green"
+// // x[1].style.color = "green"
+
+// // question 3
+// let x = document.querySelectorAll("li").style.backgroundColor = "red";
+// // x.innerText
 // console.log(x)
 
-// // question 2
 
-// let x = document.getElementsByClassName("ptag")//.style.color="green"
+Array.from(document.getElementsByTagName("li")).forEach((element)=>{
+    element.style.background ="red"
+})
 
-// x[0].style.color = "green"
-// x[1].style.color = "green"
-
-// question 3
-let x = document.querySelectorAll("li").style.backgroundColor = "red";
-// x.innerText
-console.log(x)
